e6bd995de82445fc2f23c729d6065c90b1a403d9
JangGyungSeok <juskus0310@gmail.com>
2022-10-10 16:55:58 +0900

    인프런 실전 스프링 데이터 JPA - 공통 인터페이스 검증

--- src/test/java/com/practice/javapractice2022/repository/MemberDataJpaRepositoryTest.java
+++ src/test/java/com/practice/javapractice2022/repository/MemberDataJpaRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -34,5 +36,42 @@ class MemberDataJpaRepositoryTest {
     @Test
     public void testDataJpa() {
         System.out.println("정보 확인하기 !! :" + memberDataJpaRepository.getClass());
+    }
+
+
+    // Spring Data JPA 검증
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        Member member3 = new Member("member3");
+
+        memberDataJpaRepository.save(member1);
+        memberDataJpaRepository.save(member2);
+
+        Member findMember1 = memberDataJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberDataJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 변경테스트
+//        findMember1.setUsername("username변경쓰!");
+//        Member findMember3 = memberDataJpaRepository.findById(member1.getId()).get();
+//        assertThat(findMember3.getUsername()).isEqualTo("username변경쓰!");
+
+
+        List<Member> allMember = memberDataJpaRepository.findAll();
+        assertThat(allMember.size()).isEqualTo(2);
+
+        long count = memberDataJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제검증
+        memberDataJpaRepository.delete(member1);
+        memberDataJpaRepository.delete(member2);
+
+        long deletedCount = memberDataJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
